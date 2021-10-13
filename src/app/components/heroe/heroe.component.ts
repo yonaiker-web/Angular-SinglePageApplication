fde6b8d151a41c5c7efba847c505f1b4ee13e860
Yonaiker Ocando <yonaiker2410@gmail.com>
2021-10-13 11:19:55 -0400

    Todo

---
+++ src/app/components/heroe/heroe.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HeroesService } from '../../servicios/heroes.service';
+
+ 
+
+@Component({
+  selector: 'app-heroe',
+  templateUrl: './heroe.component.html',
+})
+export class HeroeComponent {
+
+  heroe: any = {};
+
+
+  constructor( private activatedRouter: ActivatedRoute,
+               private _heroesService: HeroesService
+    ) {
+  
+    this.activatedRouter.params.subscribe( params => { 
+        this.heroe = this._heroesService.getHeroe( params['id'] );
+        console.log(this.heroe);
+     } );
+
+   }
+
+
+}
